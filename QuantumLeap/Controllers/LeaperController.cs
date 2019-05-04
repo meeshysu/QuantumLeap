e781763bee76e00b528d9bf6b162ae1af1dad202
Michelle Beshears <meeshysu@gmail.com>
2019-05-04 13:47:54 -0500

    adding a new leaper works, connects/updates in sql

--- QuantumLeap/Controllers/LeaperController.cs
+++ QuantumLeap/Controllers/LeaperController.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QuantumLeap.Data;
+using QuantumLeap.Models;
 
 namespace QuantumLeap.Controllers
 {
-    public class LeaperController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaperController : ControllerBase
     {
+        [HttpPost]
+        public ActionResult AddLeaper(CreateLeaperRequest createRequest)
+        {
+            var repository = new LeaperRepository();
+
+            var newLeaper = repository.AddLeaper(createRequest.Name,
+                                                 createRequest.BudgetAmount);
+
+            return Created($"/api/leaper/{newLeaper.Id}", newLeaper);
+        }
     }
 }
--- QuantumLeap/Data/LeaperRepository.cs
+++ QuantumLeap/Data/LeaperRepository.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using Dapper;
 using System.Threading.Tasks;
 
 namespace QuantumLeap.Data
 {
     public class LeaperRepository
     {
+        const string ConnectionString = "Server=localhost;Database=QuantumLeap;Trusted_Connection=True;";
 
+        public Leaper AddLeaper(string name, decimal budget)
+        {
+            using (var database = new SqlConnection(ConnectionString))
+            {
+                var newLeaper = database.QueryFirstOrDefault<Leaper>(
+                    @"Insert into Leapers(Name, BudgetAmount)
+                    Output inserted.*
+                    Values(@name, @budget)",
+                    new {name, budget});
+
+                if (newLeaper != null)
+                {
+                    return newLeaper;
+                }
+            }
+            throw new System.Exception("No leaper found");
+        }
     }
 }
