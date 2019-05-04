6df59aa9292e072f1e078337b3c3841a645b488e
Michelle Beshears <meeshysu@gmail.com>
2019-05-04 12:58:58 -0500

    working on setting up a request to the leaper to receive dumme data and also saving

---
+++ QuantumLeap/Models/CreateEventRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLeap.Models
+{
+    public class CreateEventRequest
+    {
+        public string Name { get; set; }
+        public DateTime EventTime { get; set; }
+        public string EventLocation { get; set; }
+        public string NameOfEvent { get; set; }
+    }
+}
---
+++ QuantumLeap/Models/Leap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLeap.Data
+{
+    public class Leap
+    {
+        public int Id { get; set; }
+        public decimal Cost { get; set; }
+        public int LeaperId { get; set; }
+        public int LeapeeId { get; set; }
+        public int EventId { get; set; }
+    }
+}
