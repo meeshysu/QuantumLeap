43d33cb37d829a5ef93fbe97060ef831753a40bf
Michelle Beshears <meeshysu@gmail.com>
2019-05-05 14:59:54 -0500

    gave up on the datetime that kept erroring out, changed to int for now to finish the rest of the requests

--- QuantumLeap/Controllers/EventController.cs
+++ QuantumLeap/Controllers/EventController.cs
@@ -3,7 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using QuantumLeap.Data;
+using QuantumLeap.Models;
 
 namespace QuantumLeap.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult AddEvent(CreateEventRequest createRequest)
+        {
+            var repository = new EventRepository();
+
+            var newEvent = repository.AddEvent(createRequest.Name,
+                                               createRequest.EventTime,
+                                               createRequest.EventLocation,
+                                               createRequest.NameOfEvent);
+
+            return Created($"/api/event/{newEvent.Id}", newEvent);
+        }
+    }
 }
